2a65e040cdce234ce02de45a9c7e45e59225e0b2
lianet <jack.tolmak@yandex.ru>
2026-05-08 20:05:19 +0300

    add contacts page tablet adaptive

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -4,14 +4,30 @@ export const initYandexMap = () => {
 
     let myMap;
 
+    const getIconParams = () => {
+        const width = window.innerWidth;
+        let size = [104, 116];
+
+        if (width <= 767) {
+            size = [78, 88];
+        } else if (width <= 1024) {
+            size = [67, 75];
+        }
+
+        return {
+            size: size,
+            offset: [-(size[0] / 2), -size[1]]
+        };
+    };
+
     const init = () => {
         const rawCoords = mapContainer.dataset.coords;
         const coords = rawCoords ? rawCoords.split(',').map(item => parseFloat(item.trim())) : [55.8528135688981, 48.842075499999964];
 
         const zoom = parseInt(mapContainer.dataset.zoom) || 16;
         const iconPath = mapContainer.dataset.icon;
-        const iconSize = mapContainer.dataset.iconSize ? mapContainer.dataset.iconSize.split(',').map(item => parseInt(item.trim())) : [104, 116];
-        const iconOffset = [-(iconSize[0] / 2), -iconSize[1]];
+
+        const iconParams = getIconParams();
 
         myMap = new ymaps.Map('map', {
             center: coords,
@@ -27,8 +43,8 @@ export const initYandexMap = () => {
             Object.assign(placemarkOptions, {
                 iconLayout: 'default#image',
                 iconImageHref: iconPath,
-                iconImageSize: iconSize,
-                iconImageOffset: iconOffset
+                iconImageSize: iconParams.size,
+                iconImageOffset: iconParams.offset
             });
         }
 
@@ -38,7 +54,10 @@ export const initYandexMap = () => {
     };
 
     const loadScript = () => {
-        if (typeof ymaps !== 'undefined') return;
+        if (typeof ymaps !== 'undefined') {
+            ymaps.ready(init);
+            return;
+        }
 
         const script = document.createElement('script');
         script.src = 'https://api-maps.yandex.ru/2.1/?apikey=4b85df55-35b1-4c23-8034-e5e8e6e58e52&lang=ru_RU';
@@ -49,7 +68,6 @@ export const initYandexMap = () => {
         };
         document.head.appendChild(script);
     };
-
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
